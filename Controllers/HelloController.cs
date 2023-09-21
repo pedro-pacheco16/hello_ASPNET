8f99ec8386db0c1f76a8b7a620eae726d6d8fd01
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-09-21 19:18:25 -0300

    Criando end point

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+   
+        [HttpGet("/bsm")]
+        public string Bsm()
+        {
+            return "Responsabilidade Pessoal\nMentalidade de Crescimento\nOrientação ao Futuro\nPersistência\nComunicação\n" + "Orientação ao Detalhe\nProatividade\nTrabalho em Equipe";
+        }
+        [HttpGet("/objetivos")]
+        public string Objetivos()
+        {
+            return "Fixar o que é uma API e como realizar uma.\nEntender os conceito de MVC e aplica-los.\nEntender sobre requisição e respostas.";
+        }
+    }
+}
